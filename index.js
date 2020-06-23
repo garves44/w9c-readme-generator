0a0a779d39c261ade6de4bfe00186f95763783b4
garves44 <garves44@gmail.com>
2020-06-23 11:40:20 -0600

    added table of conents

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
+/*********************************************************
+ * Readme Generator
+ * @package w9c-readme-generator
+ * @author Jeremy C Collins
+ * @version development
+ * @license none (public domain)
+ * ===============[ TABLE OF CONTENTS ]===================
+ * 0. Dependancies
+ * 1. Globals
+ * 2. Functions ()
+ *   2.1 writeToFile()
+ *   2.2 init()
+ * 
+ * 3. Document Ready
+ *********************************************************/
+/* ===============[ 0. Dependancies ]=====================*/
+
+
+/* ===============[ 1. Globals ]=========================*/
+
 // array of questions for user
 const questions = [
 
 ];
 
+
+/* ===============[ 2. Functions () ]======================*/
+/**
+ * 2.1 writeToFile()
+ */
+
 // function to write README file
 function writeToFile(fileName, data) {
-}
-
+};
+/**
+ * 2.2 init()
+ */
 // function to initialize program
 function init() {
 
-}
+};
+
+
+/* ===============[ 3. Document Ready ]======================*/
 
 // function call to initialize program
 init();
