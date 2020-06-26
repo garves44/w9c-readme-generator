f7e6734ed3ed4896f13a99b9f3ba66277763c689
garves44 <garves44@gmail.com>
2020-06-25 19:28:27 -0600

    added licence badge

--- index.js
+++ index.js
@@ -89,10 +89,10 @@ const questions = [
     },
   },
   {
-    type: "input",
+    type: "list",
     name: "license",
-    message:
-      'List license type for this application, for no license type "none"?(examples: GPL 3.0, MIT)(Required)',
+    message: "What is the licensing for your app?(Required)",
+    choices: ["GPL V3", "EPL 1.0", "MIT", "MPL 2.0"],
     validate: (licenseInput) => {
       if (licenseInput) {
         return true;
@@ -162,7 +162,7 @@ const questions = [
  */
 function init() {
   const toc =
-    "## Table of Contents:  \n[1. Description](#Description)  \n[2. Installation](#Installation)  \n[3. App Usage](#App-Usage)  \n[4. License](#License)  \n[5. List of Contributors](#List-of-Contributors)  \n[6. Tests](#Tests)  \n[7. Questions](#Questions)  \n";
+    "  \n ## Table of Contents:  \n[1. Description](#Description)  \n[2. Installation](#Installation)  \n[3. App Usage](#App-Usage)  \n[4. License Details](#License-Details)  \n[5. List of Contributors](#List-of-Contributors)  \n[6. Tests](#Tests)  \n[7. Questions](#Questions)  \n";
   inquirer.prompt(questions).then((res) => {
     title();
     function title() {
@@ -175,9 +175,65 @@ function init() {
             return;
           }
           console.log("Repository Name added!");
-          table();
+          licenseBadge();
         }
       );
+    }
+    function licenseBadge() {
+      if (res.license === "GPL V3") {
+        fs.appendFileSync(
+          "./dist/README.md",
+          `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
+          (err) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log("License is GPL V3!");
+            table();
+          }
+        );
+      } else if (res.license === "EPL 1.0") {
+        fs.appendFileSync(
+          "./dist/README.md",
+          `[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`,
+          (err) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log("License is EPL 1.0!");
+            table();
+          }
+        );
+      } else if (res.license === "MIT") {
+        fs.appendFileSync(
+          "./dist/README.md",
+          `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+          (err) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log("License is MIT!");
+            table();
+          }
+        );
+      } else if (res.license === "MPL 2.0") {
+        fs.appendFileSync(
+          "./dist/README.md",
+          `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`,
+          (err) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log("License is MPL 2.0!");
+            table();
+          }
+        );
+      }
+      table();
     }
     function table() {
       fs.appendFile("./dist/README.md", `${toc}`, (err) => {
@@ -231,10 +287,11 @@ function init() {
         }
       );
     }
+
     function licensing() {
       fs.appendFile(
         "./dist/README.md",
-        `## License:\n${res.license}\n`,
+        `## License Details:  \n The Eclipse Public License (EPL) is a free and open source software license most notably used for the Eclipse IDE and other projects by the Eclipse Foundation.  \n  \n Software under the GPL may be run for all purposes, including commercial purposes and even as a tool for creating proprietary software, such as when using GPL-licensed compilers.  \n  \n The MIT License is a free software license that was created at the Massachusetts Institute of Technology (MIT). It is a permissive license, meaning that it allows programmers to put the code in proprietary software on the condition that the license is given with that software, and GPL-compatible, meaning that the GPL permits programmers to combine and redistribute it with software that uses the MIT License.  \n  \n The MPL is a simple copyleft license. The MPL's "file-level" copyleft is designed to encourage contributors to share modifications they make to your code, while still allowing them to combine your code with code under other licenses (open or proprietary) with minimal restrictions. `,
         (err) => {
           if (err) {
             console.log(err);
@@ -260,23 +317,19 @@ function init() {
       );
     }
     function testing() {
-      fs.appendFile(
-        "./dist/README.md",
-        `## Tests:\n${res.tests}\n`,
-        (err) => {
-          if (err) {
-            console.log(err);
-            return;
-          }
-          console.log("Tests Listed!");
-          questioning();
+      fs.appendFile("./dist/README.md", `## Tests:\n${res.tests}\n`, (err) => {
+        if (err) {
+          console.log(err);
+          return;
         }
-      );
+        console.log("Tests Listed!");
+        questioning();
+      });
     }
     function questioning() {
       fs.appendFile(
         "./dist/README.md",
-        `## Questions:\n Here is a link to my github: https://github.com/${res.githubUser}\n Email me at:${res.contact}\n for additional questions\n`,
+        `## Questions:\n Here is a link to my github:  \nhttps://github.com/${res.githubUser}  \n Email me at:  \n${res.contact}  \nfor additional questions\n`,
         (err) => {
           if (err) {
             console.log(err);
